e8a6dbdb3faca660e7a7fc900ccb3fcc4c7dcb60
iremicyer <iremicyer@gmail.com>
2022-02-25 15:36:01 +0300

    commit-4

--- form.js
+++ form.js
@@ -1,67 +1,59 @@
-// 1) sayfanın yeniden yüklenmesini engelle
-// 2) arama alanında bulunan değeri al
-https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 
-const form = document.querySelector(".top-banner form");
-
-form.addEventListener("submit", e => {
+document.querySelector("button").addEventListener("click", e => {
+    console.log("merhaba");
     e.preventDefault();
-    const inputVal = input.value;
+    let input = document.querySelector("input");
+    getApiUserList(input);
 });
-const getApiUserList = () => {
+const getApiUserList = (input) => {
 
     $.ajax({
-
         type: "GET",
-
-        url: https://api.openweathermap.org/data/2.5/weather?q=bursa&appid=4d8fb5b93d4af21d66a2948710284366&units=metric”,
-
-            dataType: 'json',
-
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`,
+        dataType: 'json',
         success: function (veri) {
-
             console.log(veri);
 
-            if (veri[0].id != "0") {
+            // if (veri[0].id != "0") {
 
-                for (var i = 0; i < veri.length; i++) {
+            // for (var i = 0; i < veri.length; i++) {
 
-                    $('#weatherList').append(
+            //     $('#weatherList').append(
 
-                        `<tr>
-  
-                                      <td>
-  
-                                         ${veri[i].sehir}
-  
-                                      </td>
-  
-                                      <td>
-  
-                                          ${veri[i].weather}
-  
-                                      </td>
-  
-                                      <td>
-  
-                                          ${veri[i].title}
-  
-                                      </td>
-  
-                                      <td>
-  
-                                          ${veri[i].completed}
-  
-                                      </td
-                              </tr>`);
+            //         `<tr>
 
-                } }
+            //                       <td>
 
-            else {
+            //                          ${veri[i].sehir}
 
-                console.log("getApiUserList ajax send request nodata");
+            //                       </td>
 
-            }
+            //                       <td>
+
+            //                           ${veri[i].weather}
+
+            //                       </td>
+
+            //                       <td>
+
+            //                           ${veri[i].title}
+
+            //                       </td>
+
+            //                       <td>
+
+            //                           ${veri[i].completed}
+
+            //                       </td
+            //               </tr>`);
+
+            // } }
+
+            // else {
+
+            //     console.log("getApiUserList ajax send request nodata");
+
+            // }
 
         },
 
@@ -69,7 +61,7 @@ const getApiUserList = () => {
 
             // request.setRequestHeader("token", localStorage.getItem("token"));
 
-            document.getElementById("loading").style.display = "block";
+            // document.getElementById("loading").style.display = "block";
 
             console.log("getApiUserList ajax send request before");
 
@@ -79,7 +71,7 @@ const getApiUserList = () => {
 
             console.log("getApiUserList ajax send request finished");
 
-            document.getElementById("loading").style.display = "none";
+            // document.getElementById("loading").style.display = "none";
 
 
 
@@ -107,5 +99,7 @@ const getApiUserList = () => {
 
             }
         }
+
     });
+    
 }
